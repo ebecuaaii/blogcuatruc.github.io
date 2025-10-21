0dbe9d25b84e16035194b72998ae17cc77a8aca8
ebecuaaii <ngoctrucnguyen3012@gmail.com>
2025-10-21 11:05:43 +0700

    again

--- public/js/main.js
+++ public/js/main.js
@@ -98,8 +98,8 @@ document.addEventListener('DOMContentLoaded', function () {
   const faviconDarkExists = "false" === "true";
 
   if (faviconEl && faviconDarkExists) {
-    const lightFavicon = '/favicon.svg';
-    const darkFavicon = '/favicon-dark.svg';
+    const lightFavicon = '/blogcuatruc.github.io/favicon.svg';
+    const darkFavicon = '/blogcuatruc.github.io/favicon-dark.svg';
 
     const darkModeQuery = window.matchMedia("(prefers-color-scheme: dark)");
 
